d3f4834eb8e73d80d3a3eefe59103436c7165097
hadees-ahmed <hadeesahmed@yahoo.com>
2023-08-02 12:47:41 +0100

    removed extra code

--- tests/Feature/Song/SongTest.php
+++ tests/Feature/Song/SongTest.php
@@ -16,33 +16,6 @@ class SongTest extends TestCase
 {
     use DatabaseMigrations;
 
-    public function createFakeAudioFile($durationInSeconds)
-    {
-        // Create a temporary file path
-        $tempFilePath = tempnam(sys_get_temp_dir(), 'fake_audio');
-
-        // Generate a fake audio file with random data and a specific duration
-        $fakeAudioData = str_repeat(random_bytes(1024), 1024);
-        file_put_contents($tempFilePath, $fakeAudioData);
-
-        // Use getID3 to set the duration metadata of the file
-        $getID3 = new getID3();
-        $fakeAudioFileInfo = $getID3->analyze($tempFilePath);
-        $fakeAudioFileInfo['playtime_seconds'] = $durationInSeconds;
-
-        // Store the fake audio file in the storage
-        $storagePath = 'songs/' . uniqid() . '.mp3';
-        Storage::put($storagePath, $fakeAudioData);
-
-        // Delete the temporary file
-        unlink($tempFilePath);
-
-        return(object) [
-            'path' => $storagePath,
-            'duration' => $durationInSeconds,
-        ];
-    }
-
     public function test_song_is_store_in_database(): void
     {
 //        $mock = $this->mock(Audio::class, function (MockInterface $mock) {
