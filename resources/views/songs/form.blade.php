18e13f71df7993efb242ec52d563cf8f653c6239
hadees-ahmed <hadeesahmed@yahoo.com>
2023-10-12 17:50:51 +0100

    added delete and edit song features for admin panel

--- resources/views/songs/form.blade.php
+++ resources/views/songs/form.blade.php
@@ -3,10 +3,10 @@
 
 @section('content')
 <h1>Add New Song</h1>
-<form method="POST" action="{{route('songs.store')}}" enctype="multipart/form-data">
+<form method="POST" action="{{!isset($song)?route('songs.store') : route('songs.update', ['song' => $song->id])}}" enctype="multipart/form-data">
     @csrf
     <label>Song Name</label><br>
-    <input type="text" name="name" value="{{old('name')}}"><br><br>
+    <input type="text" name="name" value="{{!isset($song)? old('name') : $song->name}}"><br><br>
     @error('name')
     <p style="color: red">{{$message}}</p>
     @enderror
@@ -22,7 +22,7 @@
     <label>Artist</label><br>
     <select name="artist_id">
         @foreach($artists as $artist)
-            <option value="{{$artist->id}}">{{$artist->name}}</option>
+            <option value="{{$artist->id}}" @if(isset($song) && $artist->id === $song->artist_id) selected @endif>{{$artist->name}}</option>
         @endforeach
     </select><br><br>
 
